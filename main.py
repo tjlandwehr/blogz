7575bc7dddc3a84cb1e4de7f1f4d961dabbdbc96
tjlandwehr <tjlandwehr@gmail.com>
2017-12-06 15:10:47 -0600

    Completed main functions of blog, including creating blog posts, flashing error messages, and displaying blog posts

--- main.py
+++ main.py
@@ -19,23 +19,39 @@ class Blog(db.Model):
         self.body = body
 
     def __repr__(self):
-        return '<Title %r, Body %s>' % self.title %self.body
+        return ('<Title: %r, Body: %s>' % (self.title, self.body))
 
 def get_current_blogs():
     # List of Blog instances
     return Blog.query.all()
 
-@app.route('/blog', methods=['POST', 'GET'])
-def index():
+@app.route('/blog')
+def show_blogs():
+    return render_template('blog.html', title="Build A Blog", heading="Build a Blog", blogs=get_current_blogs())
+
+@app.route('/newpost', methods=['POST', 'GET'])
+def new_post():
+    blog_title = ""
+    blog_body = ""
 
     if request.method == 'POST':
-        blog_title = request.form['title']
-        blog_body = request.form['body']
-        new_blog = Blog(blog_title, blog_body)
-        db.session.add(new_blog)
-        db.session.commit()
+        blog_title += request.form['title']
+        blog_body += request.form['body']
+        if blog_title == "" or blog_body == "":
+            if blog_title == "":
+                flash("Please fill in the title", "title_error")
+            if blog_body == "":
+                flash("Please fill in the body", "body_error")
+            return render_template('newpost.html', title="Add Blog Entry", heading="Add a Blog Entry", 
+                blog_title=blog_title, blog_body=blog_body)
+        else:
+            new_blog = Blog(blog_title, blog_body)
+            db.session.add(new_blog)
+            db.session.commit()
+            return redirect('/blog')
 
-    return render_template('blog.html', title="Build A Blog", heading="Build a Blog", blogs=get_current_blogs())
+    return render_template('newpost.html', title="Add Blog Entry", heading="Add a Blog Entry", 
+        blog_title=blog_title, blog_body=blog_body)
 
 if __name__ == '__main__':
     app.run()
