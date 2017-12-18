694e268e8b3deabfba23c3a9e6798de3c30b57cc
tjlandwehr <tjlandwehr@gmail.com>
2017-12-18 14:28:46 -0600

    Added user-specificity for each blog post and vice versa.  Added links to see an individual user and all their posts. Added datetime to each post.

--- main.py
+++ main.py
@@ -47,25 +47,49 @@ def get_current_blogs():
     # List of Blog instances
     return Blog.query.order_by(Blog.pub_date.desc()).all()
 
+def get_current_users():
+    # List of User instances
+    return User.query.all()
+
+def get_user_blogs(username):
+    # List of blogs by a specific user
+    owner = User.query.filter_by(username=username).first()
+    return Blog.query.filter_by(owner=owner).order_by(Blog.pub_date.desc()).all()
+
 @app.before_request
 def require_login():
     allowed_routes = ['login', 'list_blogs', 'index', 'signup']
     if request.endpoint not in allowed_routes and 'username' not in session:
         return redirect('/login')
 
+@app.route('/')
+def index():
+    return render_template('index.html', title="Blogz | Blog Users", heading="Blog Users", users=get_current_users())
+
 @app.route('/blog')
 def list_blogs():
     blog_post_value = request.args.get('id')
+    userID = request.args.get('user')
     if blog_post_value:
         blog = Blog.query.get(blog_post_value)
         if not blog:
             error = "{0} is not a valid blog post ID.".format(blog_post_value)
             flash(error, "id_error")
-            return render_template('blog.html', title="Build A Blog", heading="Build a Blog", blogs=get_current_blogs())
+            return render_template('blog.html', title="Blogz | Blog Posts", heading="Blog Posts", blogs=get_current_blogs())
         else:
-            return render_template('post.html', title=blog.title, heading=blog.title, body=blog.body)
+            return render_template('post.html', title="Blogz | " + blog.title, heading=blog.title, body=blog.body, 
+                owner=blog.owner.username, date_created=blog.pub_date)
+    elif userID:
+        user = User.query.filter_by(username=userID).first()
+        if not user:
+            error = "{0} is not a valid blog user.".format(userID)
+            flash(error, "id_error")
+            return render_template('blog.html', title="Blogz | Blog Posts", heading="Blog Posts", blogs=get_current_blogs())
+        else:
+            return render_template('user.html', title="Blogz | Blog Posts by " + userID, 
+                heading="Blog Posts by " + userID, blogs=get_user_blogs(userID))
 
-    return render_template('blog.html', title="Build A Blog", heading="Build a Blog", blogs=get_current_blogs())
+    return render_template('blog.html', title="Blogz | Blog Posts", heading="Blog Posts", blogs=get_current_blogs())
 
 @app.route('/newpost', methods=['POST', 'GET'])
 def new_post():
@@ -81,7 +105,7 @@ def new_post():
                 flash("Please fill in the title", "title_error")
             if blog_body == "":
                 flash("Please fill in the body", "body_error")
-            return render_template('newpost.html', title="Add Blog Entry", heading="Add a Blog Entry", 
+            return render_template('newpost.html', title="Blogz | New Post", heading="New Post", 
                 blog_title=blog_title, blog_body=blog_body)
         else:
             new_blog = Blog(blog_title, blog_body, owner)
@@ -89,7 +113,7 @@ def new_post():
             db.session.commit()
             return redirect('/blog?id=' + str(new_blog.id))
 
-    return render_template('newpost.html', title="Add Blog Entry", heading="Add a Blog Entry", 
+    return render_template('newpost.html', title="Blogz | New Post", heading="New Post", 
         blog_title=blog_title, blog_body=blog_body)
 
 @app.route('/login', methods=['POST', 'GET'])
@@ -113,7 +137,7 @@ def login():
             flash(error, "id_error")
             return redirect('/login')
     else:
-        return render_template('login.html', title="Login to Blogz", heading="Login to Blogz")
+        return render_template('login.html', title="Blogz | Login", heading="Login to Blogz")
 
 @app.route('/logout')
 def logout():
@@ -158,7 +182,7 @@ def signup():
             username = ""
         
         if len(username_error) != 0 or len(password_error) != 0 or len(verify_error) != 0:
-            return render_template("signup.html", title="User Signup | Blogz", heading="Signup to Blogz", 
+            return render_template("signup.html", title="Blogz | User Signup", heading="Signup to Blogz", 
                 username_error=username_error and cgi.escape(username_error, quote=True),
                 password_error=password_error and cgi.escape(password_error, quote=True), 
                 verify_error=verify_error and cgi.escape(verify_error, quote=True), 
@@ -173,7 +197,7 @@ def signup():
             print(session)
             return redirect('/newpost')
     else:
-        return render_template('signup.html', title="User Signup | Blogz", heading="Signup to Blogz")
+        return render_template('signup.html', title="Blogz | User Signup", heading="Signup to Blogz")
 
 if __name__ == '__main__':
     app.run()
